c704a4a0972200437615f3a8449d5f8da824c07b
zhipingchen <zhipingchen@tencent.com>
2022-02-27 23:02:28 +0800

    合并两个有序连标

---
+++ week1/merge_two_lists/main.go
@@ -0,0 +1,36 @@
+package merge_two_lists
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	head := &ListNode{}
+	p := head
+	p1, p2 := list1, list2
+	for p1 != nil && p2 != nil {
+		if p1.Val <= p2.Val {
+			p.Next = p1
+			p1 = p1.Next
+		} else {
+			p.Next = p2
+			p2 = p2.Next
+		}
+		p = p.Next
+	}
+	if p1 != nil {
+		p.Next = p1
+	}
+	if p2 != nil {
+		p.Next = p2
+	}
+	return head.Next
+}
